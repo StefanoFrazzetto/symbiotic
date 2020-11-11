366591417add7e86d2662b388dfa2b6a3e3e1694
Stefano Frazzetto <s.frazzetto22@gmail.com>
2020-11-11 12:39:12 +0000

    Refactor to add (k)args in __init__

--- app/devices.py
+++ app/devices.py
@@ -9,7 +9,11 @@ class DeviceState(Enum):
     def __str__(self):
         return self.value
 
+
 class SmartDevice(object):
+
+    name: str
+    state: DeviceState
 
     "Map device physical states to IFTTT service_event names."
     states_events_mapping: dict = {
@@ -17,9 +21,9 @@ class SmartDevice(object):
         DeviceState.OFF: 'switch_off'
     }
 
-    def __init__(self, device_name: str) -> None:
-        self.device_name: str = device_name
-        self._current_state: DeviceState = None
+    def __init__(self, name, *args, **kwargs) -> None:
+        self.name = name
+        self.state = kwargs.get('state', None)
         self.logger = logging.getLogger(
             f'{__name__}.{self.__class__.__name__}',
         )
@@ -28,20 +32,20 @@ class SmartDevice(object):
     def _state_to_service_event(device_state: DeviceState):
         return SmartDevice.states_events_mapping[device_state]
 
+
 class LightBulb(SmartDevice):
 
-    device_name: str = "light bulb"
-
-    def __init__(self, service):
-        self._service = service
-        super().__init__(LightBulb.device_name)
+    def __init__(self, *args, **kwargs):
+        name = "light bulb"
+        self._service = kwargs.pop('service')
+        super().__init__(name, *args, **kwargs)
 
     def _change_state(self, new_state: DeviceState):
-        if new_state != self._current_state:
+        if new_state != self.state:
             try:
                 service_event = self._state_to_service_event(new_state)
                 self._service.trigger(service_event)
-                self._current_state = new_state
+                self.state = new_state
             except Exception as e:
                 message = 'Error while changing device state:\n'
                 message += f'\texception: {str(e)}\n'
