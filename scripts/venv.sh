4a3f33f78edfdf694526e35f195436b823c79560
Stefano Frazzetto <s.frazzetto22@gmail.com>
2020-11-11 13:37:28 +0000

    Improve bash scripts
    - Add correct parent dir change
    - Add information for pigpiod

---
+++ scripts/venv.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+SCRIPTS_DIR="$(dirname $0)"
+ROOT_DIR="$SCRIPT_DIR/.."
+
+# re-create venv dir
+pushd "$ROOT_DIR"
+    rm -rf venv
+    python3 -m venv venv
+    source ./venv/bin/activate
+    python -m pip install -r requirements.txt
+popd
