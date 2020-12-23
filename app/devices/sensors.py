26fefb7357add3a43db30b7796e274b2e0460d7c
Stefano Frazzetto <s.frazzetto22@gmail.com>
2020-12-23 18:30:17 +0000

    Create hooks for MotionSensor events

--- app/devices/sensors.py
+++ app/devices/sensors.py
@@ -21,13 +21,21 @@ class MotionSensor(interfaces.Loggable, ABC):
         super().__init__(*args, **kwargs)
         self.name = name
 
-    def active(self):
-        self.logger.debug(f'{self.name}: movement detected.')
-        bus.emit(f'{self.name}:active')
+    @property
+    def active(self) -> str:
+        return f'{self.name}:active'
 
-    def inactive(self):
+    @property
+    def inactive(self) -> str:
+        return f'{self.name}:inactive'
+
+    def active_hook(self):
+        self.logger.debug(f'{self.name}: movement detected.')
+        bus.emit(self.active)
+
+    def inactive_hook(self):
         self.logger.debug(f'{self.name}: movement stopped.')
-        bus.emit(f'{self.name}:inactive')
+        bus.emit(self.inactive)
 
 
 class GPIOMotionSensor(MotionSensor):
@@ -35,5 +43,5 @@ class GPIOMotionSensor(MotionSensor):
     def __init__(self, name: str, pin: int, *args, **kwargs):
         super().__init__(name, *args, **kwargs)
         self._sensor = GPIOZeroMotionSensor(pin)
-        self._sensor.when_motion = self.active
-        self._sensor.when_no_motion = self.inactive
+        self._sensor.when_motion = self.active_hook
+        self._sensor.when_no_motion = self.inactive_hook
