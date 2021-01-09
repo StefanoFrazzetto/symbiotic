4ba3b267115a6d3bf53a9a9bb3230a5ed5c04a7e
Stefano Frazzetto <stefano.frazzetto@wrld3d.com>
2021-01-09 21:21:01 +0000

    Update name in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="symbiotic-StefanoFrazzetto",
+    name="symbiotic",
     version="0.0.1",
     author="Stefano Frazzetto",
     author_email="stefano.frazzetto@hey.com",
