6eebc067b39dad7dd94988c652964c58038a5558
Stefano Frazzetto <s.frazzetto22@gmail.com>
2021-02-22 13:58:54 +0000

    Rename motion sensor methods

--- symbiotic/sensors.py
+++ symbiotic/sensors.py
@@ -4,6 +4,7 @@ from abc import ABC
 from gpiozero import MotionSensor as GPIOZeroMotionSensor
 from gpiozero.exc import BadPinFactory
 
+from .event_bus import EventBus
 from .exceptions import ConfigurationError
 
 
@@ -17,43 +18,44 @@ class MotionSensor(ABC):
     Args:
         name (str): the name to associate with the motion sensor.
     """
-    name: str
-    bus = _event_bus
-    scheduler = _scheduler
 
     def __init__(self, name: str, *args, **kwargs):
+        self.name: str = name
+        self.bus: EventBus = kwargs.pop('event_bus')
         super().__init__(*args, **kwargs)
-        self.name = name
 
     @property
-    def active(self) -> str:
+    def movement_detected(self) -> str:
         return f'{self.name}:active'
 
     @property
-    def inactive(self) -> str:
+    def movement_stopped(self) -> str:
         return f'{self.name}:inactive'
 
-    def active_hook(self):
+    def _movement_detected_hook(self):
         logging.debug(f'{self.name}: movement detected.')
-        self.bus.emit(self.active)
+        self.bus.emit(self.movement_detected)
 
-    def inactive_hook(self):
+    def _movement_stopped_hook(self):
         logging.debug(f'{self.name}: movement stopped.')
-        self.bus.emit(self.inactive)
+        self.bus.emit(self.movement_stopped)
 
 
 class GPIOMotionSensor(MotionSensor):
 
     def __init__(self, name: str, pin: int, *args, **kwargs):
-
-        try:
-            pin_factory = kwargs.pop('pin_factory', None)
-            self._sensor = GPIOZeroMotionSensor(pin, pin_factory=pin_factory)
-        except BadPinFactory:
-            err = f"Cannot instantiate {self.name} without a pin factory!"
-            raise ConfigurationError(err)
-
+        pin_factory = kwargs.pop('pin_factory', None)
+        self._initialise(pin, pin_factory)
+        self._bind_actions()
         super().__init__(name, *args, **kwargs)
 
-        self._sensor.when_motion = self.active_hook
-        self._sensor.when_no_motion = self.inactive_hook
+    def _initialise(self, pin: int, pin_factory=None) -> None:
+        try:
+            self._sensor = GPIOZeroMotionSensor(pin, pin_factory=pin_factory)
+        except BadPinFactory:
+            err = f'Cannot instantiate {self.name} without a pin factory!'
+            raise ConfigurationError(err)
+
+    def _bind_actions(self) -> None:
+        self._sensor.when_motion = self._movement_detected_hook
+        self._sensor.when_no_motion = self._movement_stopped_hook
